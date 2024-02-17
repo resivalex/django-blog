ab814071da5a95bd5a72b7666edd4fc772d2ddb8
Ivan Reshetnikov <resivalex@gmail.com>
2024-02-17 21:30:51 +0600

    English version by default

--- app/views.py
+++ app/views.py
@@ -5,11 +5,11 @@ from .resume_russian import html as russian_html
 
 
 def home(request):
-    return render(request, 'app/resume-russian.html', {'html': russian_html()})
+    return render(request, 'app/resume-english.html', {'html': english_html()})
 
 
 def english_resume(request):
-    return render(request, 'app/resume-english.html', {'html': english_html()})
+    return render(request, 'app/resume-russian.html', {'html': russian_html()})
 
 
 def styles(_request):
