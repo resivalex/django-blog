fc6013e280f6153206f6c2f7248ccf10cb43f851
Ivan Reshetnikov <resivalex@gmail.com>
2024-02-17 23:51:57 +0600

    Enable logging

--- app/settings.py
+++ app/settings.py
@@ -3,4 +3,23 @@ import random
 import string
 
 DEBUG = False
-SECRET_KEY = ''.join(random.choices(string.ascii_uppercase + string.digits, k=32))
+SECRET_KEY = "".join(random.choices(string.ascii_uppercase + string.digits, k=32))
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "ERROR",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
